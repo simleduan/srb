91169c46e40524ec031b276f1f5f676394e20e0d
fanbin-phq@sinosig.com <fanbin8807915>
2021-11-19 21:09:53 +0800

    数据字典

--- service-core/src/main/java/com/study/srb/core/service/DictService.java
+++ service-core/src/main/java/com/study/srb/core/service/DictService.java
@@ -1,9 +1,11 @@
 package com.study.srb.core.service;
 
+import com.study.srb.core.pojo.dto.ExcelDictDTO;
 import com.study.srb.core.pojo.entity.Dict;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * <p>
@@ -15,4 +17,6 @@ import java.io.InputStream;
  */
 public interface DictService extends IService<Dict> {
     void importData(InputStream inputStream);
+    List<ExcelDictDTO> listDictData();
+    List<Dict> listByParentId(Long parentId);
 }
